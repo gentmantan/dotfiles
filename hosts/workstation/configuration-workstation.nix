58cb266fe8244fbb7696baa5b52017ed0706187d
Gentman Tan <gentmantan@gmail.com>
2025-01-30 14:04:46 -0500

    Remove unnecessary xdg portal definition

--- hosts/workstation/configuration-workstation.nix
+++ hosts/workstation/configuration-workstation.nix
@@ -65,7 +65,7 @@
   services.flatpak.enable = true;
   xdg.portal = {
     enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
   users.users.tangy = { # FIXME: Change the username. Choose wisely!
