b69d650aa5379a4044d32007375b8a2859651012
Gentman Tan <gentmantan@gmail.com>
2025-02-08 10:33:49 -0500

    Use env files for containers

--- modules/containers/jdownloader.nix
+++ modules/containers/jdownloader.nix
@@ -10,15 +10,7 @@
       ];
       pull = "newer";
       extraOptions = [ "--restart=on-failure" ];
-      environment = {
-        UMASK="027";
-        USER_ID="0";
-        GROUP_ID="0";
-        SECURE_CONNECTION="1";
-        WEB_AUTHENTICATION="1";
-        WEB_AUTHENTICATION_USERNAME="user";
-        WEB_AUTHENTICATION_PASSWORD="dM2wRW1V6QlD5dSS"; # TODO: Use agenix for secrets
-      };
+      environmentFiles = [ /config/containers/jdownloader.env ];
     };
   };
 }
