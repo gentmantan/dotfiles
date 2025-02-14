8ad7541dd8e21d6f29f4a99c5fabbc0340dd29fa
Gentman Tan <gentmantan@gmail.com>
2025-02-14 11:50:02 -0500

    Add timeout and color to mako

--- modules/home-manager/mako.nix
+++ modules/home-manager/mako.nix
@@ -1,5 +1,10 @@
 {
    services.mako = {
      enable = true;
+     backgroundColor = "667777ff";
+     borderColor = "#3265ffee";
+     borderRadius = 10;
+     defaultTimeout = 10;
+     font = "Fira Code 10";
    };
 }
