6b0e4ec09d19176637c8853267ce70d246d4b4ca
Gentman Tan <gentmantan@gmail.com>
2025-03-03 09:06:24 -0500

    fixup! Replace iperf with librespeed

--- modules/containers/librespeed.nix
+++ modules/containers/librespeed.nix
@@ -3,7 +3,7 @@
     librespeed = {
       image = "ghcr.io/librespeed/speedtest";
       autoStart = true;
-      ports = [ "127.0.0.1:54573:5800" ];
+      ports = [ "127.0.0.1:54573:8080" ];
       pull = "newer";
       extraOptions = [ "--restart=on-failure" ];
     };
