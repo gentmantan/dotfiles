4a85597b92cf1458e51ba72adf02173dd0711ffc
Gentman Tan <gentmantan@gmail.com>
2025-03-03 15:01:01 -0500

    fixup! Fix remote unlocking

--- hosts/server/configuration-server.nix
+++ hosts/server/configuration-server.nix
@@ -11,7 +11,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd = {
-    availableKernelModules = [ "igc" ];
+    availableKernelModules = [ "igc" "mlx5_core" ];
     network = {
       enable = true;
       ssh = {
