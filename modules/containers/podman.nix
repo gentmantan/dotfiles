46d427f067d3de5433003f679bf7e26adc21601c
Gentman Tan <gentmantan@gmail.com>
2025-03-14 15:46:53 -0400

    Enable container image autopruning

--- modules/containers/podman.nix
+++ modules/containers/podman.nix
@@ -1,7 +1,10 @@
 {
   virtualisation = {
     containers.enable = true;
-    podman.enable = true;
+    podman = {
+      enable = true;
+      autoPrune.enable = true;
+    };
     oci-containers.backend = "podman";
   };
   systemd.timers."podman-auto-update".wantedBy = [ "timers.target" ];
