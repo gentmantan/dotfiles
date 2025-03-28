0f6d6dfa35f672c1e820d34c504dbba5ba5ba765
Gentman Tan <gentmantan@gmail.com>
2025-03-27 20:20:29 -0400

    Set time zone for microvms

--- modules/microvm/barge.nix
+++ modules/microvm/barge.nix
@@ -57,6 +57,8 @@
     services.resolved.fallbackDns = [];
     networking.firewall.interfaces."enp0s7".allowedTCPPorts = [ 16190 ];
 
+    time.timeZone = "America/New_York";
+
     system.stateVersion = "25.05";
   };
 }
--- modules/microvm/ferry.nix
+++ modules/microvm/ferry.nix
@@ -51,6 +51,9 @@
       ../ssh-server.nix
     ];
     networking.firewall.enable = false;
+
+    time.timeZone = "America/New_York";
+
     system.stateVersion = "25.05";
   };
 }
