9a2f7f108dd83f473e67097ecc68ecd5183e0030
Gentman Tan <gentmantan@gmail.com>
2025-04-04 12:32:56 -0400

    Change iperf port

--- modules/containers/iperf.nix
+++ modules/containers/iperf.nix
@@ -3,7 +3,7 @@
     iperf3 = {
       image = "docker.io/networkstatic/iperf3";
       autoStart = true;
-      ports = [ "15555:5201" ];
+      ports = [ "27374:5201" ];
       labels = {
         "io.containers.autoupdate" = "registry";
       };
