b4f64968e870cc31fff297a8077f3b731dbb00ed
Gentman Tan <gentmantan@gmail.com>
2025-05-05 22:01:20 -0400

    Fix mako

--- modules/home-manager/mako.nix
+++ modules/home-manager/mako.nix
@@ -2,11 +2,13 @@
   services.mako = {
     enable = true;
     settings = {
-      backgroundColor = "#667777ff";
-      borderColor = "#3265ffee";
-      borderRadius = "10";
-      defaultTimeout = "10";
+      background-color = "#313244AA";
+      border-color = "#313244FF";
+      border-radius = "10";
+      default-timeout = "15000";
       font = "Fira Code 10";
+      history = "0";
+      text-color = "#CDD6F4";
     };
   };
 }
