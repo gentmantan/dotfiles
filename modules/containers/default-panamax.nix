46b4862d89f3f1e94d7414bff9146cefc794991f
Gentman Tan <gentmantan@gmail.com>
2025-08-31 20:52:53 -0400

    Disable unused services

--- modules/containers/default-panamax.nix
+++ modules/containers/default-panamax.nix
@@ -1,19 +1,16 @@
 {
-  # Choose containers for panamax 
-  imports = [ 
+  # Choose containers for panamax
+  imports = [
     ./ai.nix
     ./bookstack.nix
     ./caddy.nix
     ./fireshare.nix
     ./homeassistant.nix
     ./immich.nix
-    ./iperf.nix
     ./jellyfin.nix
     ./komga.nix
     ./lanraragi.nix
-    ./librespeed.nix
     ./podman.nix
-    ./privatebin.nix
     ./pyload.nix
     ./samba.nix
     ./syncthing.nix
