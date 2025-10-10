9a7e6dfa4b42affc91816bf0d0551ce39a10afae
Gentman Tan <gentmantan@gmail.com>
2025-10-09 21:17:28 -0400

    Clean up shared-home

--- modules/home-manager/shared-home.nix
+++ modules/home-manager/shared-home.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  enableWithNushell = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
+in {
   imports = [
     ./bemenu.nix
     ./hyprcursor.nix
@@ -14,63 +19,43 @@
 
   home.stateVersion = "25.05";
 
-  home.packages = [
-    pkgs.brightnessctl
-    pkgs.grim
-    pkgs.hyprsunset
-    pkgs.nmap
-    pkgs.p7zip
-    pkgs.plantuml-c4
-    pkgs.playerctl
-    pkgs.qrencode
-    pkgs.quickemu
-    pkgs.rclone
-    pkgs.slurp
-    pkgs.wl-clipboard
-    pkgs.wl-screenrec
+  home.packages = with pkgs; [
+    brightnessctl
+    grim
+    hyprsunset
+    nmap
+    p7zip
+    plantuml-c4
+    playerctl
+    qrencode
+    quickemu
+    rclone
+    slurp
+    wl-clipboard
+    wl-screenrec
   ];
 
-  programs.home-manager.enable = true;
-  programs.imv.enable = true;
-  programs.mpv = {
-    enable = true;
-    scripts = [pkgs.mpvScripts.mpris pkgs.mpvScripts.vr-reversal];
-  };
-  programs.yt-dlp.enable = true;
-  programs.zathura = {
-    enable = true;
-    extraConfig = ''
-      set selection-clipboard clipboard
-    '';
-  };
-  programs.ripgrep.enable = true;
+  programs = {
+    home-manager.enable = true;
+    imv.enable = true;
+    mpv = {
+      enable = true;
+      scripts = [pkgs.mpvScripts.mpris pkgs.mpvScripts.vr-reversal];
+    };
+    yt-dlp.enable = true;
+    zathura = {
+      enable = true;
+      extraConfig = ''
+        set selection-clipboard clipboard
+      '';
+    };
+    ripgrep.enable = true;
 
-  # Shell utilities
-  programs.carapace = {
-    enable = true;
-    enableNushellIntegration = true;
+    # Shell utilities
+    carapace = enableWithNushell;
+    yazi = enableWithNushell;
+    starship = enableWithNushell;
+    zoxide = enableWithNushell;
   };
-  programs.yazi = {
-    enable = true;
-    enableNushellIntegration = true;
-  };
-  programs.starship = {
-    enable = true;
-    enableNushellIntegration = true;
-  };
-  programs.zoxide = {
-    enable = true;
-    enableNushellIntegration = true;
-  };
-
   services.playerctld.enable = true;
-  # services.hyprpaper = { # FIXME: Set your wallpaper here
-  #   enable = true;
-  #   settings = {
-  #     ipc = "on";
-  #     splash = false;
-  #     preload = [ "~/Pictures/ceres-fauna-nature.jpg" ];
-  #     wallpaper = [ ",~/Pictures/ceres-fauna-nature.jpg" ];
-  #   };
-  # };
 }
