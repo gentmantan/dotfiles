4d147ddd2e15fdc32f4471703204047f857c26f9
Gentman Tan <gentmantan@gmail.com>
2025-10-11 20:02:50 -0400

    Set basic-vim to two space tab

--- modules/basic-vim.nix
+++ modules/basic-vim.nix
@@ -5,6 +5,10 @@
       customRC = ''
         set number relativenumber
         filetype plugin indent on
+        set expandtab
+        set tabstop=2
+        set shiftwidth=2
+        set softtabstop=2
       '';
     };
     vimAlias = true;
