6ff964f628917fe504c11c7101c49d7fd5324d41
Gentman Tan <gentmantan@gmail.com>
2025-10-19 22:27:47 -0400

    Set restic prune rules

--- modules/restic.nix
+++ modules/restic.nix
@@ -1,22 +1,26 @@
 let
-  backup-paths = [ "/nest0/immich" "/nest0/syncthing" "/config/bookstack" "/config/env/bookstack.env" "/config/immich" "/config/games" ];
+  backup-paths = ["/nest0/immich" "/nest0/syncthing" "/config/bookstack" "/config/env/bookstack.env" "/config/immich" "/config/games"];
   keyfile = "/config/restic/backup.key";
-in
-{
+in {
   services.restic = {
     backups = {
       local-backup = {
         initialize = true;
         passwordFile = keyfile;
-        paths = backup-paths; 
+        paths = backup-paths;
         repository = "/nest1";
       };
       remote-backup = {
         initialize = true;
         environmentFile = "/config/restic/restic.env";
         passwordFile = keyfile;
-        paths = backup-paths; 
+        paths = backup-paths;
         repository = "s3:https://s3.us-east-005.backblazeb2.com/nest0-backup";
+        pruneOpts = [
+          "--keep-daily 7"
+          "--keep-weekly 5"
+          "--keep-monthly 12"
+        ];
       };
     };
   };
