b411f301300fa7e44753e1ab7be5bb092a3b563f
Gentman Tan <gentmantan@gmail.com>
2025-11-01 17:35:28 -0400

    Fix kernel module typo

--- hosts/server/configuration-server.nix
+++ hosts/server/configuration-server.nix
@@ -12,7 +12,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd = {
-    availableKernelModules = ["igc" "mlx5_core"];
+    availableKernelModules = ["igb" "mlx5_core"];
     network = {
       enable = true;
       ssh = {
