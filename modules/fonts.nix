ecc3739c48ccf51416f278415289c2854efb3381
Gentman Tan <gentmantan@gmail.com>
2025-11-01 21:08:57 -0400

    Update flake and follow noto font rename

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,12 +1,11 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-emoji
-    liberation_ttf
     fira-code
     fira-code-symbols
+    liberation_ttf
     nerd-fonts.symbols-only
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
   ];
 }
