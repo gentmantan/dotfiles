e03b312c16a2403384dd2dffaca25b0aa09d8a13
Gentman Tan <gentmantan@gmail.com>
2026-03-15 21:15:33 -0400

    Change kitty config

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -2,8 +2,7 @@
   programs.kitty = {
     enable = true;
     settings = {
-      background_blur = 5;
-      background_opacity = "0.5";
+      background_opacity = "0.7";
       confirm_os_window_close = 0;
       dynamic_background_opacity = true;
       font_family = "Noto Sans";
