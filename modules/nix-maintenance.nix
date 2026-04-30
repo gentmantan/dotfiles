04f793301f26c2efde01b29559add870cd70b90e
Gentman Tan <gentmantan@gmail.com>
2026-04-29 20:37:04 -0400

    Apply upgrade after boot

--- modules/nix-maintenance.nix
+++ modules/nix-maintenance.nix
@@ -1,30 +1,36 @@
-{ lib, config, ... }:
-let cfg = config.customNixMaintenance; in
-with lib;
 {
-  options.customNixMaintenance = {
-    enable = mkEnableOption "Enable custom maintenance options";
-    hostName = mkOption {
-      type = types.str;
-      default = "";
-      example = "workstation";
-      description = "Name of NixOS configuration to be maintained";
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.customNixMaintenance;
+in
+  with lib; {
+    options.customNixMaintenance = {
+      enable = mkEnableOption "Enable custom maintenance options";
+      hostName = mkOption {
+        type = types.str;
+        default = "";
+        example = "workstation";
+        description = "Name of NixOS configuration to be maintained";
+      };
     };
-  };
 
-  config = mkIf cfg.enable {
-    nix.optimise.automatic = true; # Automatically run nix-store --optimise to reduce nix store size
-    nix.gc = { # Automatically delete old generations
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
+    config = mkIf cfg.enable {
+      nix.optimise.automatic = true; # Automatically run nix-store --optimise to reduce nix store size
+      nix.gc = {
+        # Automatically delete old generations
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+      };
+      system.autoUpgrade = {
+        enable = true;
+        flags = ["-L"];
+        flake = "github:gentmantan/dotfiles#${cfg.hostName}";
+        operation = "boot";
+        dates = "02:00";
+        randomizedDelaySec = "45min";
+      };
     };
-    system.autoUpgrade = {
-      enable = true;
-      flake = "github:gentmantan/dotfiles#${cfg.hostName}";
-      flags = [ "-L" ];
-      dates = "02:00";
-      randomizedDelaySec = "45min";
-    };
-  };
-}
+  }
