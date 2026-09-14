6972ad19d0db15428140ac7e8966e92d2fc43905
Gentman Tan <gentmantan@gmail.com>
2026-09-14 08:48:05 -0400

    Enable terminal background blur

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -2,7 +2,8 @@
   programs.kitty = {
     enable = true;
     settings = {
-      background_opacity = "0.7";
+      background_blur = 1;
+      background_opacity = 0.7;
       confirm_os_window_close = 0;
       dynamic_background_opacity = true;
       font_family = "Maple Mono";
